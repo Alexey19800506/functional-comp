76f944c9a8aeceeaddc7e85604ab0fb106eed66d
Alexey19800506 <osiyuk19800506@gmail.com>
2023-09-04 12:00:52 +0300

    t1

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import './search.scss';
  
 const rootElement = document.querySelector('#root');
 
-const Greeting = (props) => {
+const Search = (props) => {
    const element = ( 
     <div className="search">
     <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
@@ -21,4 +21,4 @@ return element;
 
 
 
-ReactDOM.render(<Greeting name='Tom'/>, rootElement);
+ReactDOM.render(<Search name='Tom'/>, rootElement);
